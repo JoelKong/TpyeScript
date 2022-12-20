c87497d257d1a81abb499f4521e0d532399a245e
Joel Kong <joelkong2001@gmail.com>
2022-12-20 21:13:09 +0800

    feat: learnt about the different types and type inference

---
+++ types/variables.js
@@ -0,0 +1,13 @@
+var movieTitle = "Amadeus";
+movieTitle = "arrival";
+movieTitle.toUpperCase();
+var numCatLives = 9;
+var gameOver = true;
+//Type inference
+var tvShow = "Olive blablabla";
+tvShow = false;
+//the any type
+var thing = "hello";
+thing = 1;
+thing = false;
+thing();
--- types/variables.ts
+++ types/variables.ts
@@ -1,3 +1,27 @@
 let movieTitle: string = "Amadeus";
 movieTitle = "arrival";
 movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+
+let gameOver: boolean = true;
+
+//Type inference
+let tvShow = "Olive blablabla";
+tvShow = false;
+
+//the any type
+let thing: any = "hello";
+thing = 1;
+thing = false;
+thing();
+
+//infer as any
+const movies = ["asad", "asdasqweq"];
+let foundMovie: string;
+
+for (let movie of movies) {
+  if (movie === "asad") {
+    foundMovie = "Amadeus";
+  }
+}
