ec214184108266c6631bd9c94748288b559a386f
Joel Kong <joelkong2001@gmail.com>
2022-12-22 21:48:27 +0800

    feat: learnt about type aliases

--- objects.ts
+++ objects.ts
@@ -14,3 +14,14 @@ let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 function randomCoordinate(): { x: number; y: number } {
   return { x: 2, y: 1 };
 }
+
+//Type Alias
+type Point = {
+  x: number;
+  y: number;
+};
+
+let aaa: Point = { x: 34, y: 2 };
+
+type myNum = number;
+let age: myNum = 23234;
