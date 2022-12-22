1c3a8c4a3a7c788768ff70aee4e14ed458e882bc
Joel Kong <joelkong2001@gmail.com>
2022-12-22 14:16:57 +0800

    chore: completed leap year and twofer exercise

--- function.ts
+++ function.ts
@@ -23,3 +23,38 @@ function wowo(person: string = "stranger"): string {
 const add = (x: number, y: number): number => {
   return x;
 };
+
+//Annonymous function
+const colors = ["red", "orange", "yellow"];
+colors.map((color) => {});
+
+//Void (functions that dont return anything)
+function printTwice(msg: string): void {
+  console.log(msg);
+  console.log(msg);
+  return "";
+}
+
+//Exception or loops use never
+function makeError(msg: string): never {
+  throw new Error(msg);
+}
+
+function gameLoop(): never {
+  while (true) {
+    console.log("game running");
+  }
+}
+
+function twoFer(person: string = "you"): string {
+  return `One for ${person}, one for me`;
+}
+
+function isLeapyear(year: number): boolean {
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
