2b95e0cd80886f71e420fa219a48ecbf6a83db5f
Joel Kong <joelkong2001@gmail.com>
2022-12-22 13:56:49 +0800

    feat: add types for default params in funcs

--- function.ts
+++ function.ts
@@ -5,3 +5,12 @@ function square(num: number) {
 function greet(person: string) {
   return `Hi there, ${person}`;
 }
+
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
+doSomething("ChickenFace", "asd", false);
+
+//Default Parameters
+function wee(person: string = "stranger") {
+  return `Hi there, ${person}`;
+}
