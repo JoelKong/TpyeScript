8141b46943492680bd1b5f338f993b04ffbf66b7
Joel Kong <joelkong2001@gmail.com>
2022-12-23 21:29:50 +0800

    feat: learnt about array types

---
+++ arrays.ts
@@ -0,0 +1,12 @@
+const activeUsers: string[] = [];
+activeUsers.push("wee");
+
+const bools: Array<boolean> = [];
+
+type Pointt = {
+  x: number;
+  y: number;
+};
+
+const coords: Pointt[] = [];
+coords.push();
