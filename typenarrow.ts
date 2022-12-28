cfa7e717fac0b8b027bd94086c3cffd06bc29d63
Joel Kong <joelkong2001@gmail.com>
2022-12-28 17:34:55 +0800

    chore: learnt about truthiness guard

--- typenarrow.ts
+++ typenarrow.ts
@@ -4,3 +4,19 @@ function triple(value: number | string) {
   }
   return value * 3;
 }
+
+//Truthiness guard
+const el = document.getElementById("#idk");
+if (el) {
+}
+
+
+const printLetters = (word?: string) {
+    if(word) {
+        for(let char of word) {
+            console.log(char)
+        }
+    } else {
+        console.log('sdfds')
+    }
+}
