f109dd651083a25bb7e575d55ef12936e7a20e4a
Joel Kong <joelkong2001@gmail.com>
2022-12-28 14:20:31 +0800

    chore: Learnt about shorthand for parameter properties

--- class .ts
+++ class .ts
@@ -1,13 +1,16 @@
 class Playerr {
-  public readonly first: string;
-  public readonly last: string;
+  //   public readonly first: string;
+  //   public readonly last: string;
   #score: number = 0;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-    this.secretMethod();
-  }
+  //   constructor(first: string, last: string) {
+  //     this.first = first;
+  //     this.last = last;
+  //     this.secretMethod();
+  //   }
+
+  //Shortcut for parameter properties
+  constructor(public first: string, public last: string) {}
 
   //error when called outside class but still can compile to JS so #private variable still better
   private secretMethod(): void {
