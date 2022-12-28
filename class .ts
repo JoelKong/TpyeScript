de3a16b38a68c37327548145b36890953f1c363e
Joel Kong <joelkong2001@gmail.com>
2022-12-28 14:07:17 +0800

    feat: learning about ts classes

---
+++ class .ts
@@ -0,0 +1,10 @@
+class Playerr {
+  readonly first: string;
+  readonly last: string;
+  score: number = 0;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+}
