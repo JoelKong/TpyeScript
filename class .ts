8e8b33dec24db7e1b0b998ccdb9f5071084ae081
Joel Kong <joelkong2001@gmail.com>
2022-12-28 14:15:11 +0800

    chore: learnt about public and private

--- class .ts
+++ class .ts
@@ -1,10 +1,16 @@
 class Playerr {
-  readonly first: string;
-  readonly last: string;
-  score: number = 0;
+  public readonly first: string;
+  public readonly last: string;
+  #score: number = 0;
 
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
+    this.secretMethod();
+  }
+
+  //error when called outside class but still can compile to JS so #private variable still better
+  private secretMethod(): void {
+    console.log("asd");
   }
 }
