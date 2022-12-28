fa999fc306955794fe734f8a5938de07e540c4a9
Joel Kong <joelkong2001@gmail.com>
2022-12-28 16:41:49 +0800

    chore: learnt about generic functions

--- generic.ts
+++ generic.ts
@@ -1,3 +1,20 @@
 const colors: Array<string> = [];
 
 const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+
+interface Cat {
+  name: string;
+  breed: string;
+}
+
+function identity<Type>(item: Type): Type {
+  return item;
+}
+
+identity<number>(7);
+identity<Cat>({});
+
+function getRandomElement<T>(list: T[]): T {
+  const randIdx = Math.floor(Math.random() * list.length);
+  return list[randIdx];
+}
