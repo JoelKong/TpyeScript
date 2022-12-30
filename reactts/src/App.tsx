37a4542c809d5ff821255644cf083c4837d96acb
Joel Kong <joelkong2001@gmail.com>
2022-12-30 21:35:49 +0800

    chore: learnt about typescript props

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Greeter from "./components/Greeter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Greeter person="Colt" />
     </div>
   );
 }
